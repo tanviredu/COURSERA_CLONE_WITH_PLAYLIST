8e16dc4783f8e979116fef93b688e50b25462c38
tanviredu <ornobtanvir.git@gmail.com>
2020-09-27 20:51:18 +0800

    added

--- KOURSERA/App_Video/views.py
+++ KOURSERA/App_Video/views.py
@@ -7,10 +7,12 @@ from django.contrib.auth.decorators import login_required
 def get_courses_for_user(request):
     current_user = request.user
     videos = Video_Content.objects.filter(user=current_user)
+    user_videos = []
     if len(videos) > 0:
         for item in videos:
             #print(item.video_slug)
-            user_videos = Product.objects.filter(slug=item.video_slug)
+            video = Product.objects.filter(slug=item.video_slug)[0]
+            user_videos.append(video)
         print(user_videos)
         return render(request,"App_Video/course_list.html",{'user_videos':user_videos})
     else:
